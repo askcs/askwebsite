421da2d23b585717a965a5d78b0de5e7cf177074
lvdinten <lvdinten@ask-cs.com>
2014-01-27 09:52:17 +0100

    bootstrap template with slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
 console.log('\'Allo \'Allo!');
+
+
+if ($( ".item1").hasClass("active")) {
+    $( "#photo" ).delay(1000).fadeOut( 1000, function() {
+        $(".devices").show(500);
+    });
+}
+
+
+
+/*
+$( "#test" ).click(function() {
+    $( "#photo" ).fadeOut( 1000, function() {
+// Animation complete.
+    });
+});*/
